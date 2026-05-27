eafceb4c618a4f995fbca1fe058f331912776168
Huzefa Chaiwala <hchai52@gmail.com>
2026-05-27 21:52:20 +0530

    feat(employee): added db logic for create employee endpoint

--- backend/src/modules/employee/employee.repository.js
+++ backend/src/modules/employee/employee.repository.js
@@ -1,6 +1,26 @@
 const db = require("../../db");
 
-function createEmployee(data) {}
+function createEmployee(data) {
+  const stmt = db.prepare(`
+    INSERT INTO employees 
+    (first_name, last_name, email, job_title, department, country, salary)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+  `);
+
+  const result = stmt.run(
+    data.first_name,
+    data.last_name,
+    data.email || null,
+    data.job_title,
+    data.department || null,
+    data.country,
+    data.salary,
+  );
+
+  return db
+    .prepare("SELECT * FROM employees WHERE id = ?")
+    .get(result.lastInsertRowid);
+}
 
 module.exports = {
   createEmployee,
