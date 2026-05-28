2561e6f8f826d17c03da0a6b85cebbaa2bb6d2c6
Huzefa Chaiwala <hchai52@gmail.com>
2026-05-28 11:33:17 +0530

    scaffold(frontend): added env variables

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // backend URL
+  baseURL: API_BASE_URL, // backend URL
 });
 
 export default api;
